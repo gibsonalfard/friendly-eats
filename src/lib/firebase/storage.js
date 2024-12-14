b222c19aeb8492fb921d3cb6b6ff3ddead6fcc96
Ilham Gibran <ilgibrz@gmail.com>
2024-12-14 12:26:50 +0700

    Allow users to upload restaurant photo

--- src/lib/firebase/storage.js
+++ src/lib/firebase/storage.js
@@ -5,7 +5,30 @@ import { storage } from "@/src/lib/firebase/clientApp";
 import { updateRestaurantImageReference } from "@/src/lib/firebase/firestore";
 
 // Replace the two functions below
-export async function updateRestaurantImage(restaurantId, image) {}
+export async function updateRestaurantImage(restaurantId, image) {
+    try {
+        if (!restaurantId) {
+            throw new Error("Restaurant ID is required");
+        }
 
-async function uploadImage(restaurantId, image) {}
+        if (!image) {
+            throw new Error("Image is required");
+        }
+
+        publicImageUrl = await uploadImage(restaurantId, image);
+        await updateRestaurantImageReference(restaurantId, publicImageUrl);
+
+        return publicImageUrl;
+    } catch (error) {
+        console.error("Error updating restaurant image: ", error);
+    }
+}
+
+async function uploadImage(restaurantId, image) {
+    const filePath = `images/${restaurantId}/${image.name}`;
+    const imageRef = ref(storage, filePath);
+    await uploadBytesResumable(imageRef, image);
+
+    return await getDownloadURL(imageRef);
+}
 // Replace the two functions above
